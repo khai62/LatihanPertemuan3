3a94d5c5bf85557186c32a6b960a8ec181dac6fb
khairuddin <khairuddin@KHAI>
2023-10-10 11:34:31 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,340 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label10 = New Label()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        TextBox11 = New TextBox()
+        Label11 = New Label()
+        Label12 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(181, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(162, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Menghitung Nilai MahaSiswa"
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 94)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(67, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Nilai Absen"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 136)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(65, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Nilai Tugas"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(12, 180)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(54, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Nilai UTS"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(12, 225)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(56, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "NIlai UAS"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(108, 91)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(108, 128)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(108, 177)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 7
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(108, 222)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 8
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(283, 94)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(29, 15)
+        Label6.TabIndex = 9
+        Label6.Text = "10%"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(283, 136)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(29, 15)
+        Label7.TabIndex = 10
+        Label7.Text = "20%"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(283, 180)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(29, 15)
+        Label8.TabIndex = 11
+        Label8.Text = "30%"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(283, 230)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(29, 15)
+        Label9.TabIndex = 12
+        Label9.Text = "40%"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Enabled = False
+        TextBox5.Location = New Point(391, 91)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 13
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Enabled = False
+        TextBox6.Location = New Point(391, 133)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 14
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Enabled = False
+        TextBox7.Location = New Point(391, 177)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(100, 23)
+        TextBox7.TabIndex = 15
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Enabled = False
+        TextBox8.Location = New Point(391, 222)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(100, 23)
+        TextBox8.TabIndex = 16
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.GradientActiveCaption
+        Button1.Location = New Point(12, 270)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(221, 39)
+        Button1.TabIndex = 17
+        Button1.Text = "Hitung"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.GradientActiveCaption
+        Button2.Location = New Point(246, 270)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(245, 39)
+        Button2.TabIndex = 18
+        Button2.Text = "Ulang"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.GradientActiveCaption
+        Button3.Location = New Point(12, 315)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(479, 37)
+        Button3.TabIndex = 19
+        Button3.Text = "Keluar"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(12, 381)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(59, 15)
+        Label10.TabIndex = 20
+        Label10.Text = "Total Nilai"
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Enabled = False
+        TextBox9.Location = New Point(89, 378)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(78, 23)
+        TextBox9.TabIndex = 21
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Enabled = False
+        TextBox10.Location = New Point(246, 378)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(77, 23)
+        TextBox10.TabIndex = 22
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Enabled = False
+        TextBox11.Location = New Point(410, 378)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(81, 23)
+        TextBox11.TabIndex = 23
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(181, 381)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(38, 15)
+        Label11.TabIndex = 24
+        Label11.Text = "Grade"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(337, 381)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(67, 15)
+        Label12.TabIndex = 25
+        Label12.Text = "Keterangan"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(513, 484)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(TextBox11)
+        Controls.Add(TextBox10)
+        Controls.Add(TextBox9)
+        Controls.Add(Label10)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "latihan3"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+End Class
